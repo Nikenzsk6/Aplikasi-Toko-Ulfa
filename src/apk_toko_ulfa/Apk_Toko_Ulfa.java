0800847b4e88da79e9e2c97e56dcd745c4e49ef5
Nikenzsk6 <ajaniken307@gmail.com>
2025-07-09 09:18:23 +0700

    f_utama

---
+++ src/apk_toko_ulfa/Apk_Toko_Ulfa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package apk_toko_ulfa;
+
+/**
+ *
+ * @author HP
+ */
+public class Apk_Toko_Ulfa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
